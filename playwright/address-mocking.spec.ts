c5d46b3c715340343339548012286ea0bb3878f7
Petr Glaser <syreanis+1@gmail.com>
2026-07-14 03:04:11 +0200

    exercise: scaffold RÚIAN response scenarios

---
+++ playwright/address-mocking.spec.ts
@@ -0,0 +1,7 @@
+import { test } from "./fixtures";
+
+test.describe("@exercise RÚIAN response scenarios", () => {
+  test.skip("TODO: show a recoverable state for HTTP 500");
+  test.skip("TODO: show an empty state for no matches");
+  test.skip("TODO: wait for a delayed response without a fixed sleep");
+});
